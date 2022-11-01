63d6e7e3d0ac67d61685db4366e454045228f58a
Anindhita Nisitasari <anin@Anindhitas-MacBook-Air.local>
2022-11-01 23:10:02 +0700

    first commit

---
+++ 10md.js
@@ -0,0 +1,4 @@
+let stuff = ["Meja", "Buku", "Topi", "Baju", "Kayu"];
+stuff.unshift("Handuk");
+stuff.push("Celana");
+console.log(stuff)
---
+++ 11md.js
@@ -0,0 +1,3 @@
+let fruits = ["Jeruk", "Pepaya", "Jambu", "Anggur", "Melon"];
+fruits.splice(2,1);
+console.log(fruits)
